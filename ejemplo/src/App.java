56b2f4b22897c09ea8a0f16b8545667062f17df6
tomferrari517 <tomasferrari288@gmail.com>
2026-09-10 18:16:32 +0200

    Change greeting message in App class

--- ejemplo/src/App.java
+++ ejemplo/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, PSP Online");
+        System.out.println("Hello, PSP Online 2");
     }
 }
